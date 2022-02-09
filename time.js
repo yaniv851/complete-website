d52f0369727a487dfc8cb09eb7dd0adb7ca53a7c
ZribeDev <66210834+ZribeDev@users.noreply.github.com>
2022-02-09 22:22:52 +0200

    fixed

--- time.js
+++ time.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 27, 2022 15:37:25").getTime();
+var countDownDate = new Date("feb 17, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
