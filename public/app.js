459bb8f8a3db219c29f3ab7ecfcc93a397f68277
ZribeDev <66210834+ZribeDev@users.noreply.github.com>
2022-07-08 13:18:58 +0300

    all fixed (last stage before finish, I guess)

--- public/app.js
+++ public/app.js
@@ -1,16 +1,7 @@
-//   // Click submit and add doc with fields
-// addModalForm.addEventListener('submit', e => {
-//   e.preventDefault();
-//   const db = firebase.firestore();
-//   db.collection('videos').add({
-//     subject: addModalForm.subjectSel.value,
-//     book: addModalForm.bookSel.value,
-//     page: addModalForm.pageSel.value,
-//     IGLink: addModalForm.email.value,
-//   });
-// });
-
-
+const bookSel = document.getElementById("book");
+const pageSel = document.getElementById("page");
+const IGLink = document.querySelector("#IG_link");
+const formA = document.querySelector(".login-form");
 
 
 document.addEventListener("DOMContentLoaded", event => {
@@ -29,65 +20,6 @@ db.collection('videos').onSnapshot(snapshot => {
 })
 });
 
-function uploadVid(files) {
-  const file = files.item(0);
-
-  const storageRef = firebase.storage().ref();
-  //upload video to history folder inside storage.
-  const vidRef = storageRef.child('נאציזם ושואה');
-  var spaceRef = vidRef.child('page 13');
-  var random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  var ref = spaceRef.child(random).put(file);
-  //מחר השאילתא תתבצע באמצעות משפטי תנאי וכמובן לא צריך לשבור את הראש מכיוון שהקוד הנדרש כבר מוכן ונמצא בין הצ'אטים שלי ושל אייל
-  //מה שנדרש ממך זה לשים את קטע הקוד הנ"ל בתוך משפט התנאי ולהגיד שאם בחרתי במקצוע היסטוריה תעביר לתיקיה היסטוריה
-
-}
-
-const formA = document.querySelector(".login-form");
-
-var subjectObject = {
-    "History": {
-      "נאציזם ושואה": ["page 10", "page 13", "page 14"],
-      "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
-    },
-      "English": {
-      "PHP": ["12", "13", "14"]
-    }, 
-    "Math": {
-      "ahsf": ["12", "13", "14"]
-    }
-  }
-
-  
-
-
-  window.onload = function() {
-    var subjectSel = document.getElementById("subject");
-    var bookSel = document.getElementById("book");
-    var pageSel = document.getElementById("page");
-    const IGLink = document.querySelector("#IG_link");
-
-    for (var x in subjectObject) {
-      subjectSel.options[subjectSel.options.length] = new Option(x, x);
-    }
-    subjectSel.onchange = function() {
-      //empty Chapters- and Topics- dropdowns
-      pageSel.length = 1;
-      bookSel.length = 1;
-      //display correct values
-      for (var y in subjectObject[this.value]) {
-        bookSel.options[bookSel.options.length] = new Option(y, y);
-      }
-    }
-    bookSel.onchange = function() {
-      //empty Chapters dropdown
-      pageSel.length = 1;
-      //display correct values
-      var z = subjectObject[subjectSel.value][this.value];
-      for (var i = 0; i < z.length; i++) {
-        pageSel.options[pageSel.options.length] = new Option(z[i], z[i]);
-      }
-    }
     
 
     // Click submit in add modal
@@ -95,17 +27,36 @@ formA.addEventListener('submit', e => {
   e.preventDefault();
   const db = firebase.firestore();
   db.collection('videos').add({
-    subject: subjectSel.value,
-    book: bookSel.value,
-    page: pageSel.value,
-    IGLink: IGLink.value,
+    subject: subjectSel,
+    book: bookSel,
+    page: pageSel,
+    IGLink: IGLink,
   });
 
 });
 
 
+  function uploadVid(files) {
+    const file = files.item(0);
+  
+    const storageRef = firebase.storage().ref();
+    //upload video to history folder inside storage.
+    const vidRef = storageRef.child('נאציזם ושואה');
+    if(pageSel.value == "page 10"){
+      var spaceRef = vidRef.child('page 10');
+      var random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      var ref = spaceRef.child(random).put(file);
+    }
+    if(pageSel.value == "page 13"){
+      var spaceRef = vidRef.child('page 13');
+      var random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      var ref = spaceRef.child(random).put(file);
+    }
+  
+    //מחר השאילתא תתבצע באמצעות משפטי תנאי וכמובן לא צריך לשבור את הראש מכיוון שהקוד הנדרש כבר מוכן ונמצא בין הצ'אטים שלי ושל אייל
+    //מה שנדרש ממך זה לשים את קטע הקוד הנ"ל בתוך משפט התנאי ולהגיד שאם בחרתי במקצוע היסטוריה תעביר לתיקיה היסטוריה
+  
   }
-
 
   
   
